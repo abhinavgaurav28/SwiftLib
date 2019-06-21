021ecf5f173d370bf30219fff3c53a68568b8d4d
Abhinav Gaurav <abhinav.gaurav@philips.com>
2019-06-21 12:40:26 +0530

    Added test cases

---
+++ SwiftLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftLib.swift
+//  SwiftLib
+//
+//  Created by Abhinav Gaurav on 21/06/19.
+//  Copyright © 2019 Abhinav Gaurav. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
